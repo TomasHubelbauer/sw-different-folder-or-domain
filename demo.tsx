23912992783d2686a3e7f2b7ec7d620f48439d06
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-07-10 11:13:41 +0200

    Add favicon, run demos after SW install, update serve

--- demo.tsx
+++ demo.tsx
@@ -1,5 +1,3 @@
 import App from './App.tsx';
 
-window.addEventListener('load', () => {
-  ReactDOM.render(<App name="visitor" />, document.getElementById('root'));
-});
+ReactDOM.render(<App name="visitor" />, document.getElementById('root'));
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 window.addEventListener('load', async () => {
-  const registration = await navigator.serviceWorker.getRegistration();
-  if (!registration || location.host === 'localhost:5000' /* Always refresh in development */) {
-    const response = await fetch('https://tomashubelbauer.github.io/sw-js-to-ts-transpiler/worker.js');
-    const text = await response.text();
-    await navigator.serviceWorker.register('worker.js?' + btoa(text));
-  }
+  const response = await fetch('https://tomashubelbauer.github.io/sw-js-to-ts-transpiler/worker.js');
+  const text = await response.text();
+  await navigator.serviceWorker.register('worker.js?' + btoa(text));
 
-  if (!registration) {
-    // Reload the page so that the service worker transpiler comes into an effect
-    location.reload();
-  }
+  // Load up the demos after the transpiler service worker has loaded
+  await import('./demo.ts');
+  await import('./demo.tsx');
 });
